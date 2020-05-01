f34d3b50fb2fb549e7cd8bf9ff17a4fe7dde88b3
Kaizer Charania <kaizer.charania@adara.com>
2020-05-01 12:55:05 -0700

    finalize create_api

---
+++ framework/create_api/constants.py
@@ -0,0 +1,15 @@
+ERROR = dict(
+    BAD_REQUEST='Bad Request',
+    INVALID_RESPONSE='Invalid Response Format'
+)
+
+MESSAGE = dict(
+    NO_DATA_FOUND='No data found'
+)
+
+HTTP_STATUS = dict(
+    OK=200,
+    BAD_REQUEST=400
+)
+
+JOB_TTL = 48
